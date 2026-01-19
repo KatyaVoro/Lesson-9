4d6ba8d7c252054e581ee5bdaac2ed8a8587ee53
Katya <katyavoronzov@gmail.com>
2026-01-19 18:11:42 +0200

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+// let strToInject = ``;
+// const elTarget = document.getElementsByClassName('container');
+
+// data.forEach(
+//     (product) => {
+//         strToInject += `
+//         <div class="card">
+//         <h1>title</h1>
+
+
+//         `;
+//     }
+// )
+
+// ----------------------------------------
+// const data = ["aa","bb","cc"];
+
+// data.forEach(
+//     (element) => {
+//         console.log(element)
+//     }
+// );
+
+const obj = {
+    aaa: 111,
+    bbb: 222
+}
+
+localStorage.setItem("my", JSON.stringify(obj));
+
